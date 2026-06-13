cca98c9ada3f32563b0e1d84e6e0fd13c793d2f6
Nadeem Dayoub <nadeemdayoub@gmail.com>
2026-06-13 12:31:45 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/depth-of-field/",
+  base: "/qartaj-depth-of-field/",
   plugins: [react()],
 });
